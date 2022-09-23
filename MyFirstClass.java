486b4a03dba04bdde5275ec80f7e8fa367e8aad9
mirlindg <s51525@edu.campus02.at>
2022-09-23 17:10:20 +0200

    My First Class created; args as input paramater written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    // Rechtklick auf Projekt -> New -> Project Class -> Namen immer Gross beginnen und Kamel Weise schreiben
+    public static void main(String[] args) {
+        // erste Ausgabe -> psvm
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        //args
+        //sout + tab
+        System.out.println(Arrays.toString(args));
+    }
+}
