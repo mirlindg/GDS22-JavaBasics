fe0c24b8d4346532a6cca34aa53e083df13e90b1
mirlindg <s51525@edu.campus02.at>
2022-09-23 18:21:41 +0200

    Variablen definiert und Calculator zusammengebaut

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public static void main(String[] args) {
+        byte zahlEins = 20;
+        byte zahlZwei = 3;
+        int summe;
+        String calculatorName = "Taschenrechner";
+
+        summe = zahlEins + zahlZwei;
+
+        System.out.println("Erste Zahl = " + zahlEins);
+        System.out.println("Zweite Zahl = " + zahlZwei);
+        System.out.println("Die Summe = " + summe);
+        System.out.println("Der Calculator Name ist " + calculatorName);
+
+
+
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Variable Deklaration
+        String firstName;
+        int a, b, c;
+
+        a = 1;
+        b = 2;
+        c = 3; // Man könnte auch gleich int a=1, b=2, c=3; schreiben damit man die Initialisierung gleich hat
+
+        //Initialisierung
+
+        firstName = "Mirlind";
+
+        //Deklaration + Initialisierung
+        char eins = 'a';
+
+        //Variablen Wert tauschen
+        firstName = "Max";
+
+        // Wenn ich = "1" schreiben würde, könnte ich nicht mehr damit rechnen
+
+        System.out.println(firstName);
+
+
+    }
+}
