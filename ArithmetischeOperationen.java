d4b906fa1a16e4ae34f6a66b88b7ce6948e191da
mirlindg <s51525@edu.campus02.at>
2022-09-24 11:40:25 +0200

    Arithmetische Operationen sind dazu gekommen!

---
+++ ArithmetischeOperationen.java
@@ -0,0 +1,30 @@
+public class ArithmetischeOperationen {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+
+        System.out.println("Erste Zahl = " + a);
+        System.out.println("Zweite Zahl = " + b);
+
+        int addition;
+        addition = a + b;
+        System.out.println("Addition Ergebnis = " + addition);
+
+        int subtraktion;
+        subtraktion = a - b;
+        System.out.println("Subtraktion Ergebnis = " + subtraktion);
+
+        int multiplikation;
+        multiplikation = a * b;
+        System.out.println("Multiplikation Ergebnis = " + multiplikation);
+
+        int division;
+        division = a / b;
+        System.out.println("Division Ergebnis = " + division);
+
+        int modulo;
+        modulo = a % b;
+        System.out.println("Modulo Ergebnis = " + modulo);
+    }
+}
--- Calculator.java
+++ Calculator.java
@@ -2,17 +2,19 @@ public class Calculator {
     public static void main(String[] args) {
         int zahlEins = 20;
         int zahlZwei = 3;
-        int summe;
+        int summe = zahlEins + zahlZwei;
         String calculatorName = "Taschenrechner";
-
-        summe = zahlEins + zahlZwei;
 
         System.out.println("Erste Zahl = " + zahlEins);
         System.out.println("Zweite Zahl = " + zahlZwei);
         System.out.println("Die Summe = " + summe);
         System.out.println("Der Calculator Name ist " + calculatorName);
 
+       // soutv und mit Pfeiltasten
+
+        System.out.println("zahlZwei = " + zahlZwei);
 
 
     }
 }
+
